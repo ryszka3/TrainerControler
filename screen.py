27ca697b9b290872957cf8b4a20be561243f7c61
Michal Ryszka <m.ryszka@ionoptika.co.uk>
2023-12-20 20:31:54 +0000

    210122023

--- screen.py
+++ screen.py
@@ -3,10 +3,10 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-import Adafruit_ILI9341 as TFT
-import Adafruit_GPIO.SPI as SPI
+#import Adafruit_ILI9341 as TFT
+#import Adafruit_GPIO.SPI as SPI
 
-from datatypes import DataContainer
+from datatypes import DataContainer, WorkoutSegment
 
 class ScreenManager:
     
@@ -14,6 +14,8 @@ class ScreenManager:
     
     def __init__(self) -> None:
        
+
+        
         self.WIDTH = 320
         self.HEIGHT = 240
         BUS_FREQUENCY = 4000000
@@ -23,22 +25,25 @@ class ScreenManager:
         SPI_PORT = 0
         SPI_DEVICE = 0
 
-        self.COLOUR_BG: tuple      = (139, 175, 204)
+        self.X_POS_START: int  = 12
+        self.Y_POS_START: int  = 6
+
+        self.COLOUR_BG: tuple      = (31,   31,  31)
         self.COLOUR_FILL: tuple    = (139, 175, 255)
-        self.COLOUR_OUTLINE: tuple = (200, 200, 0)
-        self.COLOUR_TEXT: tuple    = (231, 223, 65)
+        self.COLOUR_OUTLINE: tuple = (208, 220, 170)
+        self.COLOUR_TEXT: tuple    = (156, 223, 250)
         self.COLOUR_BUTTON: tuple  = (200,  60, 100)
 
-        self.display = TFT.ILI9341(dc     = PIN_DC, 
-                                   rst    = PIN_RST, 
-                                   spi    = SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz = BUS_FREQUENCY), 
-                                   width  = self.WIDTH, 
-                                   height = self.HEIGHT)
-        self.display.begin()
-        self.display.clear(self.COLOUR_BG)    # Clear to black
+        #self.display = TFT.ILI9341(dc     = PIN_DC, 
+        #                           rst    = PIN_RST, 
+         #                          spi    = SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz = BUS_FREQUENCY), 
+          #                         width  = self.WIDTH, 
+           #                        height = self.HEIGHT)
+        #self.display.begin()
+        #self.display.clear(self.COLOUR_BG)    # Clear to black
 
         self.chartsData = [list([0]), list([0]), list([0]), list([0])]
-        
+        self.im = Image.new('RGB', (self.WIDTH, self.HEIGHT), self.COLOUR_BG)
 
     def assignDataContainer (self, container: DataContainer):
         self.dataContainer:DataContainer = container
@@ -65,50 +70,53 @@ class ScreenManager:
         self.display.display()
     
     def drawPageWorkout(self, workoutType:str, workoutState: str):
-        self.display.clear(self.COLOUR_BG)
-        draw = lcd.display.draw() # Get a PIL Draw object
-        X_POS_START: int  = 12
-        Y_POS_START: int  = 12
+        #self.display.clear(self.COLOUR_BG)
+        #draw = self.display.draw() # Get a PIL Draw object
+        draw = ImageDraw.Draw(self.im)
+        
+
         X_POS_CHARTS: int = 180
-        LINE_THICKNESS: int = 3
+        LINE_THICKNESS: int = 2
         Y_POS_SECTIONS = self.HEIGHT / 4    # Sections begin at 1/4 height, i.e. 240 / 4 = 60
         
         noBoxes = 3
-        box_width = (self.WIDTH - X_POS_START * (noBoxes+1))/noBoxes
+        box_width = (self.WIDTH - self.X_POS_START * (noBoxes+1))/noBoxes
+        box_height = 45
         box_Labels = (("Elapsed Time:", self.dataContainer.workoutTime, self.dataContainer.currentSegment.elapsedTime),
-                     tuple((workoutType)),
-                     "Remaining Time:", self.dataContainer.workoutDuration - self.dataContainer.workoutTime
-                                      , self.dataContainer.currentSegment.duration - self.dataContainer.currentSegment.elapsedTime)
+                     (workoutType,),
+                     ("Remaining Time:", self.dataContainer.workoutDuration - self.dataContainer.workoutTime
+                                      , self.dataContainer.currentSegment.duration - self.dataContainer.currentSegment.elapsedTime))
         
         for i in range(noBoxes):
-            box_xy = ((X_POS_START + i * (box_width + X_POS_START), Y_POS_START), 
-                      (X_POS_START + i * (box_width + X_POS_START) + box_width, self.HEIGHT / 4 - Y_POS_START))
-
-            draw.rounded_rectangle(xy = box_xy,
-                                radius = 4,
-                                fill = self.COLOUR_BG,
-                                outline = self.COLOUR_OUTLINE,
-                                width = 3)
+            box_xy = ((self.X_POS_START + i * (box_width + self.X_POS_START), self.Y_POS_START), 
+                      (self.X_POS_START + i * (box_width + self.X_POS_START) + box_width, self.Y_POS_START+box_height))
             
-            font = ImageFont.load_default(14)
-            draw.text(xy = ((box_xy[1][0]-box_xy[0][0]), Y_POS_START+5), 
+            box_centre_xy = (box_xy[0][0] + box_width / 2, box_xy[0][1] + box_height / 2)
+
+            #draw.rounded_rectangle(xy = box_xy,
+             #                   radius = 4,
+              #                  fill = self.COLOUR_BG,
+               #                 outline = self.COLOUR_BG,
+                #                width = 3)
+            
+            font = ImageFont.load_default(10)
+            draw.text(xy = (box_centre_xy[0], box_centre_xy[1]-12), 
                     text = box_Labels[i][0], # Box title
-                    fill = self.COLOUR_BG,
-                    stroke_fill = self.COLOUR_TEXT,
+                    fill = self.COLOUR_TEXT,
                     font = font,
                     anchor="mm")
             
             if i == 1:  # central box, 
                 
-                start_stop_button_dims = (50, 25)
-                start_stop_button_xy = (((self.WIDTH - start_stop_button_dims[0]) / 2, (Y_POS_SECTIONS - start_stop_button_dims[1]) / 2),
-                                        ((self.WIDTH + start_stop_button_dims[0]) / 2, (Y_POS_SECTIONS + start_stop_button_dims[1]) / 2)
+                start_stop_button_dims = (65, 20)
+                start_stop_button_xy = (((self.WIDTH - start_stop_button_dims[0]) / 2, (Y_POS_SECTIONS - start_stop_button_dims[1]) / 2 + 8),
+                                        ((self.WIDTH + start_stop_button_dims[0]) / 2, (Y_POS_SECTIONS + start_stop_button_dims[1]) / 2 + 8)
                                         )
                
                 draw.rounded_rectangle(xy = start_stop_button_xy,
-                                radius = 2,
+                                radius = 3,
                                 fill = self.COLOUR_BUTTON,
-                                outline = self.COLOUR_OUTLINE,
+                                outline = self.COLOUR_BUTTON,
                                 width = 2)
 
                 if workoutState == "FREERIDE" or workoutState == "PROGRAM":
@@ -116,29 +124,27 @@ class ScreenManager:
                 else:
                     button_label = "Resume" 
 
-                draw.text(xy = (self.WIDTH / 2, Y_POS_SECTIONS / 2), 
+                font = ImageFont.load_default(10)
+                draw.text(xy = (self.WIDTH / 2, Y_POS_SECTIONS / 2 + 8), 
                     text = button_label,
-                    fill = self.COLOUR_BUTTON,
-                    stroke_fill = self.COLOUR_TEXT,
+                    fill = self.COLOUR_TEXT,
                     font = font,
                     anchor="mm")
 
                 # no extra info to print, skip the rest of the iteration
                 continue
 
-            font = ImageFont.load_default(12)
+            font = ImageFont.load_default(9)
 
-            draw.text(xy = ((box_xy[1][0]-box_xy[0][0]), Y_POS_START+10), 
-                    text = "Total: " + box_Labels[i][1], # total
-                    fill = self.COLOUR_BG,
-                    stroke_fill = self.COLOUR_TEXT,
+            draw.text(xy = (box_centre_xy[0] - box_width / 2 + 7, box_centre_xy[1]+5), 
+                    text = "Total:    " + str(box_Labels[i][1]), # total
+                    fill = self.COLOUR_TEXT,
                     font = font,
                     anchor="lm")
             
-            draw.text(xy = ((box_xy[1][0]-box_xy[0][0]), Y_POS_START+15), 
-                    text = "Segment: "+ box_Labels[i][2], # Segment
-                    fill = self.COLOUR_BG,
-                    stroke_fill = self.COLOUR_TEXT,
+            draw.text(xy = (box_centre_xy[0] - box_width / 2 + 7, box_centre_xy[1]+17), 
+                    text = "Segment:  "+ str(box_Labels[i][2]), # Segment
+                    fill = self.COLOUR_TEXT,
                     font = font,
                     anchor="lm")
 
@@ -167,55 +173,95 @@ class ScreenManager:
                     "Zone": self.dataContainer.momentary.hrZone}}
 
         all_sections: tuple = (section1, section2, section3, section4)
+        section_height = self.HEIGHT * 3 / 4 / len(all_sections)
         
         for section in all_sections:
             
-            X_Pos: int = X_POS_START
+            X_Pos: int = self.X_POS_START + 100
 
-            draw.line(xy  = (X_Pos, Y_Pos, self.WIDTH - X_Pos, Y_Pos), 
+            draw.line(xy  = (self.X_POS_START, Y_Pos, self.WIDTH - self.X_POS_START , Y_Pos), 
                     fill  = self.COLOUR_OUTLINE, 
                     width = LINE_THICKNESS)
             
-            font = ImageFont.load_default(14)
+            font = ImageFont.load_default(11)
 
-            draw.text(xy = (X_Pos, Y_Pos),
+            draw.text(xy = (self.X_POS_START+15, Y_Pos + section_height / 2),
                     text = section["Section"],
-                    fill = self.COLOUR_BG,
-                    stroke_fill = self.COLOUR_TEXT,
+                    fill = self.COLOUR_TEXT,
                     font = font,
-                    anchor="mm")
+                    anchor="lm")
             
             X_Pos += 20
             for key in section["labels"]:
 
-                font = ImageFont.load_default(10)
-                draw.text(xy = (X_Pos, Y_Pos+32),
+                font = ImageFont.load_default(8)
+                draw.text(xy = (X_Pos, Y_Pos+35),
                         text = key,
-                        fill = self.COLOUR_BG,
-                        stroke_fill = self.COLOUR_TEXT,
+                        fill = self.COLOUR_TEXT,
                         font = font,
                         anchor="mm")
                 
 
                 font = ImageFont.load_default(16)
-                draw.text(xy = (X_Pos, Y_Pos+12),
-                        text = section["labels"][key],
-                        fill = self.COLOUR_BG,
-                        stroke_fill = self.COLOUR_TEXT,
+                draw.text(xy = (X_Pos, Y_Pos+15),
+                        text = str(section["labels"][key]),
+                        fill = self.COLOUR_TEXT,
                         font = font,
                         anchor="mm")
                 
-                # Last label is at about midpoint, so calculate spacing accordinly:
-                X_Pos += ((X_POS_CHARTS - X_POS_START) - X_POS_START) / (len(section[1]) - 1)
+                #chart area will be drawn here
+
+                # calculate spacing accordinly:
+                X_Pos += ((X_POS_CHARTS - self.X_POS_START) - self.X_POS_START) / (len(all_sections) - 1)
             
-            Y_Pos += self.HEIGHT * 3 / 4 / len(all_sections)
+            Y_Pos += section_height
+        self.im.save("workout.png")
             
 
     def drawPageMainMenu(self):
-        self.display.clear()
+        #self.display.clear()
+        #draw = self.display.draw() # Get a PIL Draw object
+        draw = ImageDraw.Draw(self.im)
+
+        noBoxes = (3, 2)    # in x and y
+        box_width  = (self.WIDTH - self.X_POS_START * (noBoxes[0]+1))/noBoxes[0]
+        box_height = box_width  *0.8 
+        box_Labels = (("Change\nUser", "History", "Settings"), ("Edit\nProgrammes","Ride\na\nProgramme", "Freeride"))
+
+        for i in range(noBoxes[1]):
+            for j in range(noBoxes[0]):
+                box_xy = ((self.X_POS_START + j * (box_width + self.X_POS_START), 
+                           self.Y_POS_START + i * (box_height + self.Y_POS_START+30)+25), 
+                          (self.X_POS_START + j * (box_width + self.X_POS_START) + box_width, 
+                           self.Y_POS_START + i * (box_height + self.Y_POS_START+30) + box_height+25))
+
+                draw.rounded_rectangle(xy = box_xy,
+                                    radius = 4,
+                                    fill = self.COLOUR_BG,
+                                    outline = self.COLOUR_OUTLINE,
+                                    width = 3)
+                
+                font = ImageFont.load_default(12)
+                box_centre_xy = (box_xy[0][0] + box_width / 2, box_xy[0][1] + box_height / 2)
+                draw.text(xy = box_centre_xy, 
+                    text = box_Labels[i][j], # Box title
+                    fill = self.COLOUR_TEXT,
+                    font = font,
+                    align="center",
+                    anchor="mm")
+                
+        self.im.save("mainMenu.png")
+
+data = DataContainer()
+data.currentSegment = WorkoutSegment("power", 24, 110)
+data.currentSegment.elapsedTime = 5
+data.workoutDuration = 60
+data.workoutTime = 20
 
 lcd = ScreenManager()
-
+lcd.assignDataContainer(data)
+#lcd.drawPageWorkout("Program", "PROGRAM")
+lcd.drawPageMainMenu()
 
 # Define a function to create rotated text.  Unfortunately PIL doesn't have good
 # native support for rotated fonts, but this function can be used to make a
