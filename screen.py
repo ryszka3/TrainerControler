89d975d650fe61dc858c2e2131cb7f035ec294f0
Michal Ryszka <m.ryszka@ionoptika.co.uk>
2024-01-13 13:51:36 +0000

    test2

--- screen.py
+++ screen.py
@@ -91,9 +91,12 @@ class TouchScreen:
     def checkTouch(self) -> tuple:
         rawTouch = self.touchscreen.get_touch()
         if rawTouch is None:
+            print("A")
             return (False, (0,0))
+            
         else:
             scaled = self.scaleCoordinates(rawTouch)
+            print("B")
             return (True, scaled)
 
 class ScreenManager:
