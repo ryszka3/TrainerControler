4785ddd9f25cdae1312f3fb6a2f40b4f736f2d07
Michal Ryszka <m.ryszka@ionoptika.co.uk>
2024-02-19 17:01:15 +0000

    buzzer implemented, no pin number yet

--- workouts.py
+++ workouts.py
@@ -6,6 +6,7 @@ import time
 import csv
 import asyncio
 import os
+import GPIO
 from   datatypes   import QueueEntry, DataContainer, WorkoutProgram, WorkoutSegment, CSV_headers
 from   BLE_Device  import FitnessMachine
 from   TCX         import TCXWriter
@@ -14,6 +15,22 @@ from   TCX         import TCXWriter
 class Buzzer:
     def __init__(self, pin) -> None:
         self.pin = pin
+        self.busy: bool = False
+        self.gpio = GPIO.get_platform_gpio()
+        self.gpio.setup(self.pin, GPIO.OUT)
+        self.gpio.output(self.pin, False)
+
+
+    async def beep(self, number_of_beeps: int, on_duration: float, period: float) -> None:
+        self.busy = True
+        for b in range(number_of_beeps):
+            self.gpio.output(self.pin, True)
+            asyncio.sleep(on_duration)
+            self.gpio.output(self.pin, False)
+            asyncio.sleep(period - on_duration)
+        self.busy = False
+
+
 
 class Workouts:
 
@@ -119,6 +136,7 @@ class WorkoutManager():
         self.writeToTCX: bool = True
         self.filename = None
         self.TCX_Object: TCXWriter = None
+        self.buzzer = Buzzer(99)
 
 
     def numberOfWorkoutPrograms(self) -> int:
@@ -248,6 +266,9 @@ class WorkoutManager():
                     try:
                         if self.dataContainer.currentSegment.elapsedTime < self.dataContainer.currentSegment.duration:
                             isSegmentTransition = False
+                        elif self.dataContainer.currentSegment.duration - self.dataContainer.currentSegment.elapsedTime <= 3 and self.buzzer.busy == False:
+                            beep_task = asyncio.create_task(self.buzzer.beep(3, 0.2, 1))
+                            
                     except:
                         pass
 
